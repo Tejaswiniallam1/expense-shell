2924e956990d67c2e319c07241532304e8d8c9e8
tejudevops <saitejaswini8121@gmail.com>
2024-11-16 16:48:42 +0530

    Created Shellscript for mysql

--- backend.sh
+++ backend.sh
@@ -9,10 +9,10 @@ rm -rf /app
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
-npm install
 unzip /tmp/backend.zip
-systemctl daemon-reload
+npm install
 
+systemctl daemon-reload
 systemctl enable backend
 systemctl start backend
 
