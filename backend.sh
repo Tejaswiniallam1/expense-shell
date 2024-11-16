4d9af6874b5bce015fec5c8a6cbcc58c4503b1a9
tejudevops <saitejaswini8121@gmail.com>
2024-11-16 16:25:28 +0530

    Created Shellscript for mysql

--- backend.sh
+++ backend.sh
@@ -1,12 +1,13 @@
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 dnf install nodejs -y
+cp Backend Service /etc/systemd/system/backend.service
+
 useradd expense
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
 npm install
-cp Backend Service /etc/systemd/system/backend.service
 unzip /tmp/backend.zip
 systemctl daemon-reload
 
