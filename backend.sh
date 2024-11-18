7bc65929035b7d66e1d9671bd02854d85e20e432
tejudevops <saitejaswini8121@gmail.com>
2024-11-18 09:44:22 +0530

    Created Shellscript for backend

--- backend.sh
+++ backend.sh
@@ -1,44 +1,47 @@
 log_file=/tmp/expense.log
 MYSQL_PASSWORD=$1
-echo -e  "\e[35m Disable nodejs existing version\e[0m"
+Head(){
+  echo -e "\e[$1\e[0m"
+}
+Head  "Disable nodejs existing version"
 dnf module disable nodejs -y &>>log_file
 
-echo -e  "\e[35m Enable NODEJS18 Version\e[0m"
+Head  "Enable NODEJS18 Version"
 dnf module enable nodejs:18 -y &>>log_file
 
-echo -e  "\e[35m install NODEJS\e[0m"
+Head  "install NODEJS"
 dnf install nodejs -y &>>log_file
 
-echo -e  "\e[35m Configure Backend Service\e[0m"
+Head  "Configure Backend Service"
 cp backend.service /etc/systemd/system/backend.service &>>log_file
 
-echo -e  "\e[35m add application user\e[0m"
+Head  "Add application user"
 useradd expense &>>log_file
 
-echo -e  "\e[35m remove existing App content\e[0m"
+Head  "remove existing App content"
 rm -rf /app &>>log_file
 
-echo -e  "\e[35m create application directory\e[0m"
+Head  "create application directory"
 mkdir /app &>>log_file
 
-echo -e  "\e[35m download Application content\e[0m"
+Head  "download Application content"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>log_file
 cd /app &>>log_file
 
-echo -e  "\e[35m extracting the application content\e[0m"
+Head  "extracting the application content"
 unzip /tmp/backend.zip &>>log_file
 
-echo -e  "\e[35m downloading application dependencies\e[0m"
+Head  "downloading application dependencies"
 npm install &>>log_file
 
-echo -e "\e[35m reloading the systemd and start backend server\e[0m"
+Head "reloading the systemd and start backend server"
 systemctl daemon-reload &>>log_file
 systemctl enable backend &>>log_file
 systemctl start backend &>>log_file
 
-echo -e "\e[35m install mysql client\e[0m"
+Head "install mysql client"
 dnf install mysql -y &>>log_file
 
-echo -e "\e[35m load schema\e[0m"
+Head "load schema"
 mysql -h mysql-dev.tejudevops.online -uroot -p${MYSQL_PASSWORD} < /app/schema/backend.sql &>>log_file
 ## >/dev/null -- this command used to not store the data into the disk.
