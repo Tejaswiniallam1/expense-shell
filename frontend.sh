284bb5f20e88ba2e4babc5f080b135bd620b766d
tejudevops <saitejaswini8121@gmail.com>
2024-11-18 12:25:28 +0530

    Created Shellscript for backend

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,31 @@
+log_file=/tmp/expense.log
+MYSQL_PASSWORD=$1
+
+Head(){
+  echo -e "\e[35m$1\e[0m"
+}
+Head "Install Nginx Server"
 dnf install nginx -y
+echo $?
+
+Head "Copy expense config file"
 cp expense.conf /etc/nginx/default.d/expense.conf
+echo $?
+
+Head "Remove the old/default content"
 rm -rf /usr/share/nginx/html/*
+echo $?
+
+Head "Download Application code"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
+echo $?
+
+Head "Extract Application code"
 unzip /tmp/frontend.zip
+echo $?
+
+Head "Start Nginx Service"
 systemctl enable nginx
 systemctl restart nginx
+echo $?
